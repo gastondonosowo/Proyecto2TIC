e81fe221afab66ad27ff44f76242253a60c0c8bf
Chocatrip <alejandro.diaz@mail.udp.cl>
2021-06-09 20:51:27 -0400

    Firebase

--- Arduino/CapturaDatos.py
+++ Arduino/CapturaDatos.py
@@ -1,5 +1,8 @@
 import serial
 import time
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
 arduino = serial.Serial('COM4',9600)
 time.sleep(2)
@@ -8,3 +11,19 @@ while(i > 0):
     dato = arduino.readline()
     print(dato)
 arduino.close()
+
+cred = credentials.Certificate('firebase-sdk.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://proyecto-tics-2-default-rtdb.firebaseio.com/'
+
+})
+
+ref = db.reference('/')
+ref.set({
+    'Datos':
+        {
+            'pruebadatos': {
+                        'dato': dato
+            }
+        }
+})
